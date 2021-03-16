e6700ac063cdd20a506b3afac2cae31733d1bdfd
Eunel Soliveres <eunel_soliveres@yahoo.com>
2021-03-16 23:56:14 +0800

    pig Gamez

---
+++ batch6-activities/pigJS.js
@@ -0,0 +1,155 @@
+var playerX = "";
+var playerY = "";
+var player1 = "";
+var player2 = "";
+var play1 = true;
+var play2 = false;
+var count1 = 0;
+var count2 = 0;
+var total1 = 0;
+var total2 = 0;
+var skirmish = false;
+var campaign = false;
+var conquest = false;
+var winner = "";
+
+
+function playersX(name) {
+	playerX = name;
+}
+
+function playersY(name) {
+	playerY = name;
+}
+
+function firstPlayer(playerX,playerY) {
+	let pos1 = Math.floor(Math.random() * (7 - 1) + 1);
+	let pos2 = Math.floor(Math.random() * (7 - 1) + 1);
+	for (let i = 0; i <= 2; i++) {
+		if (pos1 > pos2) {
+			player1 = playerX;
+			player2 = playerY;
+			console.log(player1);
+			console.log(player2);
+			break;
+		} else if (pos1 < pos2) {
+			player1 = playerY;
+			player2 = playerX;
+			console.log(player1);
+			console.log(player2);
+			break;
+		} else i--;
+	}
+}
+
+function skirmishSelect() {
+	skirmish = true;
+	return skirmish;
+}
+
+function campaignSelect() {
+	campaign = true;
+	return campaign;
+}
+
+function conquestSelect() {
+	conquest = true;
+	return conquest;
+}
+
+function diceRoll() {
+	if (play1) {
+		console.log(player1);
+		let count = Math.floor(Math.random() * (7 - 1) + 1);
+		if (count === 1) {
+			console.log(0);
+			changePlayer();
+		} else {
+			count1 += count;
+			return count1;
+		}
+	} else if (play2) {
+		console.log(player2);
+		let count = Math.floor(Math.random() * (7 - 1) + 1);
+		if (count === 1) {
+			console.log(0);
+			changePlayer();
+		} else {
+			count2 += count;
+			return count2;
+		}
+	}
+}
+
+function holdCount() {
+	if (play1) {
+		total1 += count1;
+		console.log(total1);
+		if (skirmish) skirmishMatch();
+		else if (campaign) campaignMatch();
+		else if (conquest) conquestMatch();
+	} else if (play2) {
+		total2 += count2;
+		console.log(total2);
+		if (skirmish) skirmishMatch();
+		else if (campaign) campaignMatch();
+		else if (conquest) conquestMatch();
+	}
+}
+
+function skirmishMatch() {
+	if (play1) {
+		console.log(total1);
+		if (total1 >= 30) {
+			winner = player1;
+			return winner;
+		} else changePlayer();
+	} else if (play2) {
+		console.log(total2);
+		if (total2 >= 30) {
+			winner = player2;
+			return winner;
+		} else changePlayer();
+	} 
+}
+
+function campaignMatch() {
+	if (play1) {
+		if (total1 >= 60) {
+			winner = player1;
+			return winner;
+		} else changePlayer();
+	} else if (play2) {
+		if (total2 >= 60) {
+			winner = player2;
+			return winner;
+		} else changePlayer();
+	} 
+}
+
+function conquestMatch() {
+	if (play1) {
+		if (total1 >= 100) {
+			winner = player1;
+			return winner;
+		} else changePlayer();
+	} else if (play2) {
+		if (total2 >= 100) {
+			winner = player2;
+			return winner;
+		} else changePlayer();
+	} 
+}
+
+function changePlayer() {
+	if (play1) {
+		count1 = 0;
+		play1 = false;
+		play2 = true;
+	} else if (play2) {
+		count2 = 0;
+		play2 = false;
+		play1 = true;
+	}
+}
+
