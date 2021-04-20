ea47828f77a1bf8486207cefe26d4b72d544b2e7
Eunel Soliveres <eunel_soliveres@yahoo.com>
2021-04-20 21:39:37 +0800

    added spin

--- batch6-activities/shortReels/youtubeAPI.js
+++ batch6-activities/shortReels/youtubeAPI.js
@@ -7,13 +7,18 @@ var movieList = ['Space Jam: A New Legacy', 'The Mitchells vs. the Machines', 'T
 var YTkey = 'AIzaSyCn_-EZ7BeA4izRYj58lvnuXC0YhJaCGP8';
 var YTplaylistId = 'PLhDTzN_1ZTYCOMSbJKosoLpmezKvbvvBy';
 var YTURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
-
 var options = {
 	part : 'snippet',
 	key : YTkey,
 	maxResults : 20,
 	playlistId : YTplaylistId 
 }
+
+const API_KEY = 'api_key=21d8144b59aa5bdd7a78dde28a1c6e5b';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
 var OMDBkey = 'cbe59045';
 var OMDBresult = '';
@@ -40,11 +45,78 @@ function getMovieData(movieNo) {
 			console.log(data);
 			document.getElementById('movieTitle').innerHTML = data.Title;
 			document.getElementById('movieYear').innerHTML = data.Year;
+			document.getElementById('movieActors').innerHTML = data.Actors;
+			document.getElementById('moviePlot').innerHTML = data.Plot;
 			if (data.Runtime === 'N/A') document.getElementById('movieRuntime').innerHTML = '0 min';
 			else document.getElementById('movieRuntime').innerHTML = data.Runtime;
 		}
 	})
 }
+
+function getMovies(url) {
+
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+        if(data.results.length !== 0){
+            showMovies(data.results);
+        }else{
+            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+        }
+       
+    })
+
+}
+
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+             <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+
+            <div class="overview">
+
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        
+        `
+
+        main.appendChild(movieEl);
+    })
+}
+
+function getColor(vote) {
+    if(vote>= 8){
+        return 'green'
+    }else if(vote >= 5){
+        return "orange"
+    }else{
+        return 'red'
+    }
+}
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     const searchTerm = search.value;
+//     selectedGenre=[];
+//     setGenre();
+//     if(searchTerm) {
+//         getMovies(searchURL+'&query='+searchTerm)
+//     }else{
+//         getMovies(API_URL);
+//     }
+
+// })
 
 function getTime(movieNo) {
 	document.getElementById('overlay').style.opacity = '1';
@@ -84,7 +156,7 @@ function getTime(movieNo) {
 
 function mainVid(id) {
 	$('#video').html(`
-		<iframe class="ytVid" src="https://www.youtube.com/embed/${id}?rel=0&enablejsapi=1"></iframe>
+		<iframe class="ytVid" src="https://www.youtube.com/embed/${id}?rel=0&enablejsapi=1&mute=0"></iframe>
 	`);
 }
 
@@ -129,7 +201,7 @@ $(document).ready(function() {
 		document.getElementById('reelLayer').style.display = 'none';
 		}, 2000);
 	}, 2000);
-
+	getMovies(API_URL);
 	let movieNo = Math.floor(Math.random() * 10) + 0;
 	globalCheck = movieNo
 	loadVids(movieNo);
@@ -137,4 +209,4 @@ $(document).ready(function() {
 	document.getElementById('cookieBG').style.backgroundColor = 'rgba(4,4,4,0.75)'; }, 1000);
 });
 
-//21d8144b59aa5bdd7a78dde28a1c6e5b
+// https://www.youtube.com/watch?v=9Bvt6BFf6_U
