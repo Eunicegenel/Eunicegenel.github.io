dff1aec20a82e78fafddf17f3ae809eb7d6f1869
Eunel Soliveres <eunel_soliveres@yahoo.com>
2021-04-20 11:34:46 +0800

    new trailers

--- batch6-activities/shortReels/youtubeAPI.js
+++ batch6-activities/shortReels/youtubeAPI.js
@@ -1,92 +1,138 @@
-var key = 'AIzaSyCn_-EZ7BeA4izRYj58lvnuXC0YhJaCGP8';
-var playlistId = 'PLhDTzN_1ZTYCOMSbJKosoLpmezKvbvvBy';
-var URL = 'https://www.googleapis.com/youtube/v3/playlistItems';
+var cookieCheck = false;
+var globalCheck = 0;
+var globalTimeout;
+var movieList = ['Space Jam: A New Legacy', 'The Mitchells vs. the Machines', 'The Suicide Squad', 'Ghostbusters: Afterlife', 'Godzilla vs. Kong', 'Luca', 'Loki', 'Raya and the Last Dragon', 'Cruella', 'The Unholy'];
+
+
+var YTkey = 'AIzaSyCn_-EZ7BeA4izRYj58lvnuXC0YhJaCGP8';
+var YTplaylistId = 'PLhDTzN_1ZTYCOMSbJKosoLpmezKvbvvBy';
+var YTURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
 var options = {
 	part : 'snippet',
-	key : key,
+	key : YTkey,
 	maxResults : 20,
-	playlistId : playlistId 
+	playlistId : YTplaylistId 
 }
 
+var OMDBkey = 'cbe59045';
+var OMDBresult = '';
+
 function loadVids(movieNo) {
-	$.getJSON(URL, options, function(data) {
-		console.log(data);
+	let time = getTime(movieNo);
+	$.getJSON(YTURL, options, function(data) {
 		var id = data.items[movieNo].snippet.resourceId.videoId;
-		console.log(id);
-		let time = 0;
-
-		switch(movieNo) {
-			case 0:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/spaceJam2.png")';
-				time = 143000;
-				break;
-			case 1:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/mitchells.png")';
-				time = 140000;
-				break;
-			case 2:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/suicideSquad.png")';
-				time = 139000;
-				break;
-			case 3:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/ghostbusters.png")';
-				time = 140000;
-				break;
-			case 4:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/jupiterLegacy.png")';
-				time = 143000;
-				break;
-			case 5:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/bigfootFamily.png")';
-				time = 112000;
-				break;
-			case 6:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/loki.png")';
-				time = 132000;
-				break;
-			case 7:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/rayaDragon.png")';
-				time = 141000;
-				break;
-			case 8:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/cruella.png")';
-				time = 116000;
-				break;
-			case 9:
-				document.getElementById('test').style.backgroundImage = 'url("reelAsset/theUnholy.png")';
-				time = 145000;
-				break;
-		}
-
-		document.getElementById('test').setAttribute('onclick','controlVideo("playVideo",1,'+time+');');
 		mainVid(id);
+		clearTimeout(globalTimeout);
+		getMovieData(movieNo);
+		if (cookieCheck) { setTimeout(function(){ playVideo('playVideo',time); }, 2000); }
 	})
+}
+
+function getMovieData(movieNo) {
+	let movieTitle = movieList[movieNo];
+	var OMDBurl = 'http://www.omdbapi.com/?apikey='+OMDBkey;
+	
+	$.ajax({
+		method:'GET',
+		url:OMDBurl+'&t='+movieTitle+'&y='+2021,
+		success:function(data){
+			console.log(data);
+			document.getElementById('movieTitle').innerHTML = data.Title;
+			document.getElementById('movieYear').innerHTML = data.Year;
+			if (data.Runtime === 'N/A') document.getElementById('movieRuntime').innerHTML = '0 min';
+			else document.getElementById('movieRuntime').innerHTML = data.Runtime;
+		}
+	})
+}
+
+function getTime(movieNo) {
+	document.getElementById('overlay').style.opacity = '1';
+	switch(movieNo) {
+		case 0:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/spaceJam2.png")';
+			return time = 143000;
+		case 1:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/mitchells.png")';
+			return time = 139000;
+		case 2:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/suicideSquad.png")';
+			return time = 138000;
+		case 3:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/ghostbusters.png")';
+			return time = 139000;
+		case 4:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/gvk.png")';
+			return time = 143000;
+		case 5:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/luca.png")';
+			return time = 83000;
+		case 6:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/loki.png")';
+			return time = 132000;
+		case 7:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/rayaDragon.png")';
+			return time = 140500;
+		case 8:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/cruella.png")';
+			return time = 116000;
+		case 9:
+			document.getElementById('overlay').style.backgroundImage = 'url("reelAsset/theUnholy.png")';
+			return time = 145000;
+	}
 }
 
 function mainVid(id) {
 	$('#video').html(`
-		<iframe class="ytVid" src="https://www.youtube.com/embed/${id}?rel=0&enablejsapi=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+		<iframe class="ytVid" src="https://www.youtube.com/embed/${id}?rel=0&enablejsapi=1"></iframe>
 	`);
 }
 
-function controlVideo(vidcontrol,choice,time) {
+function playVideo(vidcontrol,time) {
     var div = document.getElementById("video");
     var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
     iframe.postMessage('{"event":"command","func":"' + vidcontrol + '","args":""}', '*');
-    document.getElementById('test').style.backgroundColor = 'rgba(4,4,4,0)';
-    if(choice === 1) {
-    	setTimeout(function(){ document.getElementById('test').style.opacity = '0'; }, 1000);
-    	setTimeout(function(){ document.getElementById('test').style.opacity = '1'; 
-		controlVideo('pauseVideo',0,0);}, time);
-		setTimeout(function(){ document.getElementById('test').style.opacity = '1'; }, time-500);
-		document.getElementById('test').setAttribute('onclick','none()');
-    }
+    setTimeout(function(){ document.getElementById('overlay').style.opacity = '0'; }, 2000);
+    globalTimeout = setTimeout(function(){ pauseVideo('pauseVideo'); }, time);
 }
 
-function none (){}
+function pauseVideo(vidcontrol) {
+    document.getElementById('overlay').style.opacity = '1';
+    setTimeout(function(){ 
+    	var div = document.getElementById("video");
+	    var iframe = div.getElementsByTagName("iframe")[0].contentWindow;
+	    iframe.postMessage('{"event":"command","func":"' + vidcontrol + '","args":""}', '*');
+	}, 1000);
+}
+
+function closeCookie() {
+	document.getElementById('cookieM').style.display = 'none';
+	document.getElementById('cookieBG').style.display = 'none';
+}
+
+document.getElementById("cookieM").addEventListener("click", function(){
+	let time = getTime(globalCheck);
+	cookieCheck = true;
+    setTimeout(function(){ playVideo('playVideo',time); }, 2000);
+});
+
+document.getElementById("cookieBG").addEventListener("click", function(){
+	let time = getTime(globalCheck);
+	cookieCheck = true;
+    setTimeout(function(){ playVideo('playVideo',time); }, 2000);
+});
 
 $(document).ready(function() {
+	setTimeout(function(){ 
+	document.getElementById('reelLayer').style.opacity = '0'; 
+		setTimeout(function(){ 
+		document.getElementById('reelLayer').style.display = 'none';
+		}, 2000);
+	}, 2000);
+
 	let movieNo = Math.floor(Math.random() * 10) + 0;
-	loadVids(movieNo);	
+	globalCheck = movieNo
+	loadVids(movieNo);
+	setTimeout(function(){ 
+	document.getElementById('cookieBG').style.backgroundColor = 'rgba(4,4,4,0.75)'; }, 1000);
 });
